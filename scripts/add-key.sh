d44b5d458e6c5db7a5430dd9905d6f04193b1f7a
Vladimir Ignatev <ya.na.pochte@gmail.com>
2016-03-12 15:25:46 +0400

    proper chmod for scripts

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -21,3 +21,8 @@ security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keycha
 # Put the provisioning profile in place
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp "./scripts/profile/$PROFILE_NAME.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
+
+if [ $? -ne 0 ]; then
+    echo "Unable to copy provision profile"
+    exit 20
+fi
