d170ca54fe8719ce74f37c8b251c2cb92239fc91
Vladimir Ignatev <ya.na.pochte@gmail.com>
2016-03-12 15:08:04 +0400

    Added code signing for Travis builds

---
+++ scripts/sign-and-upload.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
+OUTPUTDIR="$PWD/build/Release-iphoneos"
+
+xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
