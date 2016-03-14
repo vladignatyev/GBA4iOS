9e28b43f76e165719824a6f40fa92dcaafd89a54
Vladimir Ignatev <ya.na.pochte@gmail.com>
2016-03-14 14:16:13 +0400

    travis ci: removed useless system output in release notes

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -2,33 +2,21 @@
 PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
 OUTPUTDIR="$PWD/build/Enterprise-iphoneos"
 
-echo $PWD
-echo $OUTPUTDIR
-
 mkdir /Users/travis/build/vladignatyev/GBA4iOS/build/Enterprise-iphoneos/ipa
 
 RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
 
 read -r -d '' RELEASE_NOTES <<- EOM
-  RELEASE NOTES
-  =============
-  Build: $TRAVIS_BUILD_NUMBER
-  Release date: $RELEASE_DATE
-
-  Repo: $TRAVIS_REPO_SLUG
-  Branch: $TRAVIS_BRANCH
-  Commit: $TRAVIS_COMMIT
-
-  System info
-  -----------
-  TRAVIS_XCODE_SDK: $TRAVIS_XCODE_SDK
-  TRAVIS_XCODE_SCHEME: $TRAVIS_XCODE_SCHEME
-  TRAVIS_XCODE_PROJECT: $TRAVIS_XCODE_PROJECT
-  TRAVIS_XCODE_WORKSPACE: $TRAVIS_XCODE_WORKSPACE
+  RELEASE NOTES\r\n
+  =============\r\n
+  Build: $TRAVIS_BUILD_NUMBER\r\n
+  Release date: $RELEASE_DATE\r\n
+\r\n
+  Repo: $TRAVIS_REPO_SLUG\r\n
+  Branch: $TRAVIS_BRANCH\r\n
+  Commit: $TRAVIS_COMMIT\r\n
 EOM
 
 echo $RELEASE_NOTES > $OUTPUTDIR/ipa/notes.txt
-
-echo $OUTPUTDIR/ipa/notes.txt
 
 xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/ipa/$APP_NAME - $TRAVIS_BUILD_NUMBER.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
